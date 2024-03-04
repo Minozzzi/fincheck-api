58f2094ac43f077e0633dd9f27e5caaccbb275ad
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-04 10:30:27 -0300

    feat(users): add getUserById endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ActiveUserId } from 'src/shared/decorators/active-user-id.decorator';
 
 @Controller('users')
 export class UsersController {
-  constructor() {}
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
+  }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 
 @Injectable()
 export class UsersService {
-  constructor() {}
+  constructor(private readonly usersRepo: UsersRepository) {}
+
+  async getUserById(id: string) {
+    return this.usersRepo.findUnique({
+      where: { id },
+      select: {
+        email: true,
+        name: true,
+      },
+    });
+  }
 }
