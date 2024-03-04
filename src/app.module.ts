fc4088881683ba2f43ee1bca779bd3777a198420
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-04 20:20:44 -0300

    feat(bankAccounts): add module and all endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,18 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { BankAccountsResolver } from './modules/bank-accounts/bank-accounts.resolver';
+import { BankAccountsModule } from './modules/bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, CategoriesModule, BankAccountsModule],
   controllers: [],
   providers: [
     {
       provide: APP_GUARD,
       useClass: AuthGuard,
     },
+    BankAccountsResolver,
   ],
 })
 export class AppModule {}
